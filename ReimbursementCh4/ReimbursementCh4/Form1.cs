0739c326e50132153660800bc85cace8098ea49f
Oway13 <wyattcody133@gmail.com>
2019-07-31 11:00:30 -0500

    Chapter 4: Reimbursement Exercise Completed

---
+++ ReimbursementCh4/ReimbursementCh4/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReimbursementCh4
+{
+    public partial class Form1 : Form
+    {
+
+        int startingMileage, endingMileage;
+        double milesTraveled, amountOwed;
+        double reimburseRate = 0.39;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startingMileage = (int) numericUpDown1.Value;
+            endingMileage = (int)numericUpDown2.Value;
+            if(startingMileage > endingMileage)
+            {
+                MessageBox.Show("The starting mileage must be less than the ending mileage.", "Cannot Calculate");
+            }
+            else
+            {
+                milesTraveled = endingMileage - startingMileage;
+                amountOwed = milesTraveled * reimburseRate;
+                label4.Text = "$" + amountOwed;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(milesTraveled + "miles", "Miles Traveled");
+        }
+    }
+}
