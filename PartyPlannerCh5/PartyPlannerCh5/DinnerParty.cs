e29aa87f8aa4125bf866fc21586b214c8e920975
Oway13 <wyattcody133@gmail.com>
2019-08-04 23:58:28 -0500

    Chapter 5: Party Planner Exercise - Finished DinnerParty class

---
+++ PartyPlannerCh5/PartyPlannerCh5/DinnerParty.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyPlannerCh5
+{
+    class DinnerParty
+    {
+        public const int COST_OF_FOOD_PER_PERSON = 25;
+
+        public int NumberOfPeople;
+        public decimal CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations;
+        public bool healthyOption;
+        public bool fancyOption;
+
+        public void SetHealthyOption(bool healthyOption) {
+            this.healthyOption = healthyOption;
+        }
+
+        public void CalculateCostOfDecorations()
+        {
+            if (fancyOption)
+            {
+                CostOfDecorations = 15M * NumberOfPeople + 50M;
+            }
+            else
+            {
+                CostOfDecorations = 7.5M * NumberOfPeople + 30M;
+            }
+        }
+
+
+        public decimal CalculateCost()
+        {
+            if (healthyOption)
+            {
+                CostOfBeveragesPerPerson = 5M;
+                return ((CostOfBeveragesPerPerson+COST_OF_FOOD_PER_PERSON) * NumberOfPeople + CostOfDecorations)*0.95M;
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20M;
+                return (CostOfBeveragesPerPerson + COST_OF_FOOD_PER_PERSON) * NumberOfPeople + CostOfDecorations;
+            }
+        }
+    }
+}
