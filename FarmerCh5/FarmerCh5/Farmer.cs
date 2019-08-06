ad76182d55e70cda433be50e62432bbcd3ddc02c
Oway13 <wyattcody133@gmail.com>
2019-08-06 10:11:37 -0500

    Chapter 5: Farmer Exercise Completed

---
+++ FarmerCh5/FarmerCh5/Farmer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmerCh5
+{
+    class Farmer
+    {
+        public int BagsOfFeed { get; private set; }
+
+        private int feedMultiplier;
+        public int FeedMultiplier { get { return feedMultiplier; } }
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = FeedMultiplier * numberOfCows;
+            }
+        }
+
+        public Farmer(int numberOfCows, int feedMultiplier)
+        {
+            this.feedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
+    }
+}
