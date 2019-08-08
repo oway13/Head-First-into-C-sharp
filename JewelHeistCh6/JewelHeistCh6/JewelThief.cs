e2e4fd7ab1a4eeb9c41c6514a02d7a5c81c59b47
Oway13 <wyattcody133@gmail.com>
2019-08-08 10:36:52 -0500

    Chapter 6: Jewel Heist Exercise Completed

---
+++ JewelHeistCh6/JewelHeistCh6/JewelThief.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelHeistCh6
+{
+    class JewelThief : Locksmith
+    {
+        private Jewels stolenJewels = null;
+
+        public override void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine("I'm stealing the contents! " + stolenJewels.Sparkle());
+        }
+    }
+}
