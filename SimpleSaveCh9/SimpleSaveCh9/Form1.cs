79c121599e80d51e942bdd5b01793c198fc198e7
Oway13 <wyattcody133@gmail.com>
2019-08-20 13:01:23 -0500

    Chapter 9: Simple Text Saving Exercise Completed

---
+++ SimpleSaveCh9/SimpleSaveCh9/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SimpleSaveCh9
+{
+    public partial class Form1 : Form
+    {
+        private string name;
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.InitialDirectory = @"C:\Desktop\c# learning\";
+            openFileDialog1.InitialDirectory = @"C:\Desktop\c# learning\";
+
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+            openFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = openFileDialog1.FileName;
+                textBox1.Clear();
+                textBox1.Text = File.ReadAllText(name);
+                Text = name;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = saveFileDialog1.FileName;
+                File.WriteAllText(name, textBox1.Text);
+                Text = name;
+            }
+
+        }
+    }
+}
