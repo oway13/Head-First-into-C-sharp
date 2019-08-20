a672963b4f2fe9f9feb334bec5e619c8de41e8f0
Oway13 <wyattcody133@gmail.com>
2019-08-20 14:34:03 -0500

    Chapter 9: Card Deck Serialize Exercise Completed

---
+++ DeckSaveCh9/DeckSaveCh9/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckSaveCh9
+{
+    [Serializable]
+    class Card
+    {
+
+        public Card(Suits suit, Values value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        public Suits suit;
+        public Values value;
+        public string Name
+        {
+            get
+            {
+                return value.ToString() + " of " + suit.ToString();
+            }
+        }
+
+        public static string Plural(Values value)
+        {
+            if (value == Values.Six)
+                return "Sixes";        
+            else            
+                return value.ToString() + "s";
+        }
+    }
+}
