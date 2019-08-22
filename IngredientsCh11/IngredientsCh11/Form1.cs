45379ca65b8909319a752dd0f310886e44ec85e3
Oway13 <wyattcody133@gmail.com>
2019-08-22 13:50:26 -0500

    Chapter 11: Secret Ingredient Delegate Exercise Completed

---
+++ IngredientsCh11/IngredientsCh11/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IngredientsCh11
+{
+    public partial class Form1 : Form
+    {
+        GetSecretIngredient ingredientMethod = null;
+        Suzanne suzanne = new Suzanne();
+        Amy amy = new Amy();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ingredientMethod != null)
+                Console.WriteLine("I'll add " + ingredientMethod((int)numericUpDown1.Value));
+            else
+                Console.WriteLine("I don't have a secret ingredient");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ingredientMethod = new GetSecretIngredient(suzanne.MySecretIngredientMethod);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ingredientMethod = new GetSecretIngredient(amy.AmysSecretIngredientMethod);
+        }
+    }
+}
