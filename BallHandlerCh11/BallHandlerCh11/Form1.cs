7e16176ee7dd88ef7832ea1e18c9cca953b6532f
Oway13 <wyattcody133@gmail.com>
2019-08-22 11:10:42 -0500

    Chapter 11: Ball Event Handler Exercise Completed

---
+++ BallHandlerCh11/BallHandlerCh11/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallHandlerCh11
+{
+    public partial class Form1 : Form
+    {
+        Ball ball;
+        Pitcher pitcher;
+        Fan fan;
+        public Form1()
+        {
+            InitializeComponent();
+            ball = new Ball();
+            pitcher = new Pitcher(ball);
+            fan = new Fan(ball);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BallEventArgs ballEventArgs = new BallEventArgs((int)numericUpDown1.Value, (int)numericUpDown2.Value);
+            ball.OnBallInPlay(ballEventArgs);
+        }
+    }
+}
---
+++ BallHandlerCh11/BallHandlerCh11/Pitcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallHandlerCh11
+{
+    class Pitcher
+    {
+        public Pitcher(Ball ball)
+        {
+            ball.BallInPlay += new EventHandler(Ball_BallInPlay);
+        }
+
+        private void Ball_BallInPlay(object sender, EventArgs e)
+        {
+            if (e is BallEventArgs)
+            {
+                BallEventArgs ballEventArgs = e as BallEventArgs;
+                if ((ballEventArgs.Distance < 95) && ballEventArgs.Trajectory < 60)
+                    CatchBall();
+                else
+                    CoverFirstBase();
+            }
+        }
+
+        public void CatchBall()
+        {
+            Console.WriteLine("The pitcher caught the ball");
+        }
+
+        public void CoverFirstBase()
+        {
+            Console.WriteLine("The pitcher ran to first base");
+        }
+    }
+}
