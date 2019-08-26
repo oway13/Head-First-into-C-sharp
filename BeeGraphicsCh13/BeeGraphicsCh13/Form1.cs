0ace67854f5b8a2d0fc861492fe5b937c74fa76f
Oway13 <wyattcody133@gmail.com>
2019-08-26 09:59:40 -0500

    Chapter 13: Bee Graphics Exercise Part 1 Completed

---
+++ BeeGraphicsCh13/BeeGraphicsCh13/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeeGraphicsCh13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = CreateGraphics())
+            using (Pen thickBlackPen = new Pen(Brushes.Black, 3.0F))
+            using(Font font = new Font("Arial", 16, FontStyle.Italic))
+            {
+                g.FillRectangle(Brushes.SkyBlue, ClientRectangle);
+                g.DrawImage(Properties.Resources.Bee_animation_1, 50, 20, 75, 75);
+                g.DrawImage(Properties.Resources.Flower, 10, 130, 100, 150);
+                g.DrawLines(thickBlackPen, new Point[] {
+                            new Point(130, 110), new Point(120, 160), new Point(155, 163)});
+                g.DrawLines(thickBlackPen, new Point[] {
+                            new Point(120, 160), new Point(175, 120), new Point(215, 70)});
+                SizeF size = g.MeasureString("Nectar here", font);
+                g.DrawString("Nectar here", font, Brushes.Red, new Point(215 - (int)size.Width / 2, 70 - (int)size.Height));
+            }
+        }
+
+        private void DrawBee(Graphics g, Rectangle rect)
+        {
+            g.DrawImage(Properties.Resources.Bee_animation_1, rect);
+        }
+    }
+}
