72e048ec8d278aba9fc76ca8e827d761699b56be
Oway13 <wyattcody133@gmail.com>
2019-08-26 10:24:12 -0500

    Chapter 13: Image Zoomer Exercise Completed

---
+++ ZoomCh13/ZoomCh13/Zoomer.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ZoomCh13
+{
+    partial class Zoomer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.Color.White;
+            this.trackBar2.Location = new System.Drawing.Point(255, 0);
+            this.trackBar2.Maximum = 175;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 256);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Value = 175;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.White;
+            this.trackBar1.Location = new System.Drawing.Point(0, 255);
+            this.trackBar1.Maximum = 175;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(256, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 175;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Zoomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Zoomer";
+            this.Size = new System.Drawing.Size(300, 300);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Zoomer_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
---
+++ ZoomCh13/ZoomCh13/Zoomer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZoomCh13
+{
+    public partial class Zoomer : UserControl
+    {
+        Bitmap photo;
+        public Zoomer()
+        {
+            InitializeComponent();
+            photo = new Bitmap(Properties.Resources.ice_ele);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Zoomer_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(new Pen(Brushes.White), ClientRectangle);
+            e.Graphics.DrawImage(photo, 10, 10, trackBar1.Value, trackBar2.Value);
+        }
+    }
+}
