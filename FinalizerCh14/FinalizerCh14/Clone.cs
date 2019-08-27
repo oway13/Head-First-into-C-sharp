e67597ff2a0fed825627f5152bd24db7d5cd346c
Oway13 <wyattcody133@gmail.com>
2019-08-27 11:46:39 -0500

    Chapter 14: Finalizer & GC Exercise Completed

---
+++ FinalizerCh14/FinalizerCh14/Clone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalizerCh14
+{
+    class Clone : IDisposable
+    {
+        public int Id { get; private set; }
+
+        public Clone(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public void Dispose()
+        {
+            MessageBox.Show("I've been disposed!", "Clone #" + Id + " says...");
+        }
+
+        ~Clone()
+        {
+            MessageBox.Show("Aaargh! You got me!", "Clone #" + Id + " says...");
+        }
+    }
+}
